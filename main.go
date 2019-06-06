c39ff275409e10398cefd18a4bb39de72651ca58
tomocy <tomocy.dev@gmail.com>
2019-06-07 00:39:29 +0900

    clone command separating some resources

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := cloneCommand("/bin/sh")
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to run command: %s\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func cloneCommand(name string, args ...string) *exec.Cmd {
+	cmd := exec.Command(name, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+	}
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+
+	return cmd
+}
