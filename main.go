22e3ee0e4e83982af7ce981371471d3b0875dd7e
tomocy <tomocy.dev@gmail.com>
2019-06-10 18:35:06 +0900

    mount /proc to /proc with proc type to separate proc id

--- main.go
+++ main.go
@@ -60,7 +60,9 @@ func showUsage() {
 func clone(args ...string) error {
 	cmd := exec.Command("/proc/self/exe", args...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET |
+			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS,
 		UidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
@@ -91,6 +93,11 @@ func initAndExec(name string) error {
 
 func initAsContainer() error {
 	if err := syscall.Sethostname([]byte("container")); err != nil {
+		return err
+	}
+	if err := syscall.Mount("/proc", "/proc", "proc", uintptr(
+		syscall.MS_NOEXEC|syscall.MS_NOSUID|syscall.MS_NODEV,
+	), ""); err != nil {
 		return err
 	}
 
