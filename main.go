e6e67a6abfb803150243c50e1604e733ad09f503
tomocy <tomocy.dev@gmail.com>
2019-06-10 15:59:22 +0900

    set uts on container

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -8,19 +9,56 @@ import (
 )
 
 func main() {
-	cmd := cloneCommand("/bin/sh")
-	if err := cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to run command: %s\n", err)
-		os.Exit(1)
+	config := parseConfig()
+	switch config.command {
+	case cmdClone:
+		if err := clone("-init"); err != nil {
+			fmt.Printf("failed to clone: %s\n", err)
+			os.Exit(1)
+		}
+	case cmdInit:
+		if err := initAndExec("/bin/sh"); err != nil {
+			fmt.Printf("failed to init and exec: %s\n", err)
+			os.Exit(1)
+		}
+	default:
+		showUsage()
 	}
-
-	os.Exit(0)
 }
 
-func cloneCommand(name string, args ...string) *exec.Cmd {
-	cmd := exec.Command(name, args...)
+func parseConfig() *config {
+	isSetUp := flag.Bool("init", false, "init self as container")
+	flag.Parse()
+	cmd := cmdClone
+	if *isSetUp {
+		cmd = cmdInit
+	}
+
+	return &config{
+		command: cmd,
+	}
+}
+
+type config struct {
+	command command
+}
+
+const (
+	_ command = iota
+	cmdClone
+	cmdInit
+)
+
+type command int
+
+func showUsage() {
+	flag.Usage()
+}
+
+func clone(args ...string) error {
+	cmd := exec.Command("/proc/self/exe", args...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS,
 		UidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
@@ -38,5 +76,21 @@ func cloneCommand(name string, args ...string) *exec.Cmd {
 	}
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 
-	return cmd
+	return cmd.Run()
+}
+
+func initAndExec(name string) error {
+	if err := initAsContainer(); err != nil {
+		return err
+	}
+
+	return syscall.Exec(name, []string{name}, os.Environ())
+}
+
+func initAsContainer() error {
+	if err := syscall.Sethostname([]byte("container")); err != nil {
+		return err
+	}
+
+	return nil
 }
