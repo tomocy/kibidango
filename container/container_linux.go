7242ef0ff71b5911d8d1f3514005c3a025201028
tomocy <tomocy.dev@gmail.com>
2019-06-11 11:00:23 +0900

    change root

--- container/container_linux.go
+++ container/container_linux.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"io"
 	"os"
 	"os/exec"
 	"syscall"
@@ -52,11 +53,54 @@ func buildCloneCommand(args ...string) *exec.Cmd {
 }
 
 func (c *LinuxContainer) load(name string) error {
+	if err := c.prepare(); err != nil {
+		return err
+	}
 	if err := c.init(); err != nil {
 		return err
 	}
 
 	return syscall.Exec(name, []string{name}, os.Environ())
+}
+
+func (c *LinuxContainer) prepare() error {
+	if err := os.MkdirAll("/root/container/proc", 0777); err != nil {
+		return err
+	}
+	if err := os.MkdirAll("/root/container/bin", 0777); err != nil {
+		return err
+	}
+	if err := os.MkdirAll("/root/container/lib", 0777); err != nil {
+		return err
+	}
+	if err := copyFile("/lib/ld-musl-x86_64.so.1", "/root/container/lib/ld-musl-x86_64.so.1"); err != nil {
+		return err
+	}
+	if err := copyFile("/bin/sh", "/root/container/bin/sh"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func copyFile(src, dest string) error {
+	input, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer input.Close()
+
+	output, err := os.OpenFile(dest, os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	if _, err := io.Copy(output, input); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *LinuxContainer) init() error {
@@ -66,6 +110,12 @@ func (c *LinuxContainer) init() error {
 	if err := syscall.Mount("/proc", "/proc", "proc", uintptr(
 		syscall.MS_NOEXEC|syscall.MS_NOSUID|syscall.MS_NODEV,
 	), ""); err != nil {
+		return err
+	}
+	if err := syscall.Chroot("/root/container"); err != nil {
+		return err
+	}
+	if err := syscall.Chdir("/"); err != nil {
 		return err
 	}
 
