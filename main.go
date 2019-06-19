07bc96af2389f0bd9c1ded8dd38119ceac0ffd11
tomocy <tomocy.dev@gmail.com>
2019-06-19 12:23:14 +0900

    use client to run a container

--- main.go
+++ main.go
@@ -4,18 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/tomocy/kibidango/config"
-	"github.com/tomocy/kibidango/engine/booter"
-	"github.com/tomocy/kibidango/engine/container"
-	"github.com/tomocy/kibidango/engine/launcher"
+	"github.com/tomocy/kibidango/client/cli"
 )
 
 func main() {
-	conf := config.Parse()
-	lcher := launcher.ForOS(conf.OS, os.Stdin, os.Stdout, os.Stderr)
-	bter := booter.ForOS(conf.OS, "/root/container")
-	cner := container.New(lcher, bter)
-	if err := cner.Start(conf); err != nil {
+	client := cli.New()
+	if err := client.Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to run: %s\n", err)
 		os.Exit(1)
 	}
