94caae67434eaee6379286712d30e2cabc93bd18
tomocy <tomocy.dev@gmail.com>
2019-07-02 23:06:40 +0900

    add a method to kibidango to expose its id

--- kibidango.go
+++ kibidango.go
@@ -6,6 +6,10 @@ type kibidango struct {
 	id string
 }
 
+func (k *kibidango) ID() string {
+	return k.id
+}
+
 func (k *kibidango) updateID(id string) error {
 	if id == "" {
 		return errors.New("id should not be empty")
