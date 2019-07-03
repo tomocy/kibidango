4ea5a339b6f616441da6ddbb47c6e7221b228dae
tomocy <tomocy.dev@gmail.com>
2019-07-03 20:50:57 +0900

    add method to list all states of kibidango

--- factory/factory.go
+++ factory/factory.go
@@ -2,24 +2,41 @@ package factory
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
-func createWorkspace(id string) error {
-	dir := filepath.Join(workSpacesDir, id)
-	return os.MkdirAll(dir, 0777)
+func list() ([]*state, error) {
+	srces, err := read(workSpacesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	states := make([]*state, len(srces))
+	for i, src := range srces {
+		if !src.IsDir() {
+			continue
+		}
+
+		state, err := load(src.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		states[i] = state
+	}
+
+	return states, nil
 }
 
-func save(state *state) error {
-	name := filepath.Join(workSpacesDir, state.ID, "state.json")
-	dest, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, 0777)
-	if err != nil {
-		return err
+func read(dir string) ([]os.FileInfo, error) {
+	var infos []os.FileInfo
+	if _, err := os.Stat(dir); err != nil {
+		return infos, nil
 	}
-	defer dest.Close()
 
-	return json.NewEncoder(dest).Encode(state)
+	return ioutil.ReadDir(dir)
 }
 
 func load(id string) (*state, error) {
@@ -36,6 +53,22 @@ func load(id string) (*state, error) {
 	}
 
 	return state, nil
+}
+
+func createWorkspace(id string) error {
+	dir := filepath.Join(workSpacesDir, id)
+	return os.MkdirAll(dir, 0777)
+}
+
+func save(state *state) error {
+	name := filepath.Join(workSpacesDir, state.ID, "state.json")
+	dest, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	return json.NewEncoder(dest).Encode(state)
 }
 
 type state struct {
